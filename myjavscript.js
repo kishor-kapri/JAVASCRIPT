b529d06a862b9e49a8a45e98302bc4fe432b725c
kishor-kapri <102507309+kishor-kapri@users.noreply.github.com>
2022-03-28 13:12:37 +0530

    Add files via upload

---
+++ myjavscript.js
@@ -0,0 +1,27 @@
+function onTweetAdd() {
+    alert('Tweet Added');
+
+    const tweet = document.getElementById("one");
+    console.log(tweet.value);
+
+}
+const tweet = document.getElementById("one");
+console.log(tweet);
+
+console.log(document.getElementById("one"));
+
+const ankiObject = {
+    name: 'Anki',
+    age: 10,
+    class: '12th',
+    height: '6ft',
+    getElementById: function onTweetAdd() {
+        alert('Tweet Added')
+    }
+}
+
+
+/**
+ * create a function jokitext area ki value read krk console m print kr suke 
+ *   
+ */
